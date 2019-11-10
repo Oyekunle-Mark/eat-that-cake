e39b3f2feec7842e8f074567a5eb2c464ee15a74
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-10 18:09:32 +0100

    Properly doc it

--- arrays/mergin_meeting_times.py
+++ arrays/mergin_meeting_times.py
@@ -3,19 +3,30 @@ def merge_ranges(meetings):
     meetings_list = sorted(meetings)
     # set the first meeting to an array of the first tuple in meetings
     ordered_meetings = [meetings_list[0]]
+
     # loop through the meeting for start and end times
     for start_time, end_time in meetings_list:
         # set the previous start and end times to the last item in the meeting array
         previous_start, previous_end = ordered_meetings[-1]
-        # check if
+
+        # check if the end time of the previous meeting overlaps
+        # or touches the start of the current meeting
         if previous_end >= start_time:
+            # if it swallows up this meeting altogether
             if previous_end >= end_time:
+                # do nothing
                 continue
+            # otherwise
             else:
+                # update the end time of the previous meeting
                 ordered_meetings[-1] = (previous_start, end_time)
+        # otherwise, if this meeting does not overlap
+        # with previous meeting in any way
         else:
+            # add it
             ordered_meetings.append((start_time, end_time))
 
+    # return the merged ranges
     return ordered_meetings
 
 
