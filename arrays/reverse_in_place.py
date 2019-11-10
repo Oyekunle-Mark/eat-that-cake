b638b14e83ee517ae313c50e1d087e68e4408efb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-10 18:16:20 +0100

    reverse in place solution

---
+++ arrays/reverse_in_place.py
@@ -0,0 +1,5 @@
+def reverse_in_place(char_ls):
+    # get the length of the char_ls - 1 and set to a variable length
+    # loop through the char_ls for the index and char in char_ls
+        # swap the character at index with the charcter at length - index
+    pass
