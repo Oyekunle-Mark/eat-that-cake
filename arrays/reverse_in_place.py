5809bb04a3c58b3eedbba68217385048c56f3dfb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-10 18:39:23 +0100

    First pass solution

--- arrays/reverse_in_place.py
+++ arrays/reverse_in_place.py
@@ -1,5 +1,20 @@
 def reverse_in_place(char_ls):
-    # get the length of the char_ls - 1 and set to a variable length
-    # loop through the char_ls for the index and char in char_ls
-        # swap the character at index with the charcter at length - index
-    pass
+    # get the length of the char_ls - 1 and set to a variable right_index
+    right_index = len(char_ls) - 1
+    # set the index to zero
+    left_index = 0
+
+    # while left_index is less than right_index
+    while left_index < right_index:
+        # swap the character at left_index with the character at right_index
+        char_ls[left_index], char_ls[right_index] = char_ls[right_index], char_ls[left_index]
+
+        # increment index
+        left_index += 1
+        # decrement length
+        right_index -= 1
+
+
+string = ["h", "e", "l", "l", "o"]
+reverse_in_place(string)
+print(string)
