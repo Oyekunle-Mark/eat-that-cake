aa63d8f3f46ad115672f57c5abe82d6870f61d8f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-10 19:14:44 +0100

    Pseudocode

---
+++ arrays/merge_sorted.py
@@ -0,0 +1,24 @@
+def merge_sorted(ls1, ls2):
+    # set a variable pos1 to zero
+    # set a variable pos2 to zero
+    # set a variable len1 to length of ls1
+    # set a variable len2 to length of ls2
+    # create a variable sorted_list and initialize to empty list
+    # loop while pos1 is less than len1 and pos2 is less than len2
+        # if item at index pos1 of ls1 is less than
+        # item at index pos2 of ls2
+            # append item at index pos1 of ls1 to sorted_list
+            # increment pos1
+        # otherwise, append item at index pos2 to sorted_list
+            # increment pos2
+
+    # loop while pos1 is less than len1
+        # append item at index pos1 of ls1 to sorted_list
+        # increment pos1
+
+    # loop while pos2 is less than len2
+        # append item at index pos2 of ls2to sorted_list
+        # increment pos2
+
+    # return sorted_list
+    pass
