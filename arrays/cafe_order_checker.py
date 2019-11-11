e3e57b50210f095c198f6909ed35ce0afe55a051
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-11 18:59:52 +0100

    Cafe order checker pseudocode

--- arrays/cafe_order_checker.py
+++ arrays/cafe_order_checker.py
@@ -1,2 +1,19 @@
-def cafe_order_checker(take_out_orders, dine_in_orders, served_orders):
+def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
+    # initialize take_out_index to zero
+    # initialize dine_in_index to zero
+    # initialize take_out_len to the len of take_out_orders - 1
+    # initialize dine_in_len to the len of dine_in_orders - 1
+    # loop through server_orders
+        # if take_out_index is less than take_out_len and order is at index
+        # take_out_index of take_out_orders
+            # increment take_out_index
+        # also, if dine_in_index is less than dine_in_len and order is at index
+        # dine_in_index of dine_in_orders
+            # increment dine_in_index
+        # otherwise,
+            # return False
+    # check if take_out_index does not equal take_out_len
+    # and dine_in_index does not equal dine_in_len
+        # return False
+    # return True
     pass
