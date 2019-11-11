d15fb36fcdad327435e8cabb48d7035b510f4802
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-11 09:26:44 +0100

    First pass solutiion

--- hash_tables/inflight_entertainment.py
+++ hash_tables/inflight_entertainment.py
@@ -1,9 +1,15 @@
 def inflight_entertainment(flight_length, movie_lengths):
     # initialize a variable movies to an empty dictionary
+    movies = {}
     # loop through movie_lengths
+    for movie in movie_lengths:
         # check to see if flight_length minus the current movie is in the movies
+        if flight_length - movie in movies:
             # if it is, return True
+            return True
         # otherwise, add the current movie to movies as a key value pair
+        else:
+            movies[movie] = movie
 
     # return False if no match is found
-    pass
+    return False
