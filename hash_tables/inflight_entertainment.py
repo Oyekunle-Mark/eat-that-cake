1657e6b51237722504a93060e8f4dbc865b527f6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-11 09:25:09 +0100

    Inflight entertainment pseudocode

--- hash_tables/inflight_entertainment.py
+++ hash_tables/inflight_entertainment.py
@@ -1,2 +1,9 @@
 def inflight_entertainment(flight_length, movie_lengths):
+    # initialize a variable movies to an empty dictionary
+    # loop through movie_lengths
+        # check to see if flight_length minus the current movie is in the movies
+            # if it is, return True
+        # otherwise, add the current movie to movies as a key value pair
+
+    # return False if no match is found
     pass
