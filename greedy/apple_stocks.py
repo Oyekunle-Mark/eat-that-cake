dc9a12f898d3e38d8a67a0fe772c6181e220e60d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-13 09:49:41 +0100

    Apple stocks pseudocodd

--- greedy/apple_stocks.py
+++ greedy/apple_stocks.py
@@ -1,2 +1,11 @@
 def get_max_profit(stock_prices):
+    # initialize minimum_stock_price to the first price in stock_prices
+    # initialize max_profit to the difference between first and second prices
+    # loop through the stock_prices
+        # check if the current price is less than minimum_stock_price
+            # replace minimum_stock_price with current stock
+        # check if the difference between current price and minimum stock
+        # price is greater than max_profit
+            # replace max_profit with the difference
+    # return max_profit
     pass
