c5337b03653d3465b7fd2e985ba619db053dc9c1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-13 09:56:48 +0100

    Tested the first pass

--- greedy/apple_stocks.py
+++ greedy/apple_stocks.py
@@ -18,3 +18,11 @@ def get_max_profit(stock_prices):
             max_profit = current_max_profit
     # return max_profit
     return max_profit
+
+
+print(get_max_profit([10, 7, 5, 8, 11, 9]))  # 6
+print(get_max_profit([1, 5, 3, 2]))  # 4
+print(get_max_profit([7, 2, 8, 9]))  # 7
+print(get_max_profit([1, 6, 7, 9]))  # 8
+print(get_max_profit([9, 7, 4, 1]))  # -2
+print(get_max_profit([1, 1, 1, 1]))  # 0
