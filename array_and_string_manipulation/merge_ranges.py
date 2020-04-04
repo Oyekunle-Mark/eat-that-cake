4d6d8c76df8e0b87515fadec85c5e6aaa7d67481
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-04 18:31:05 +0100

    Pseudocode for first pass solution

---
+++ array_and_string_manipulation/merge_ranges.py
@@ -0,0 +1,14 @@
+def merge_ranges(meetings):
+    # create a new copy of the meetings and sort it in the process
+    # initialize a variable ret to am empty list
+    # initialize variables start and end to the intergers
+    # in the first tuple in meetings
+    # loop through meetings
+        # compare end to the start time of current meeting
+        # if end is greater than or equal to start time
+            # set end to the end time of current meeting
+        # otherwise, if end is less than start time
+            # append start time and end to ret as a tuple
+            # set start and end to the start and end times of next meeting
+    # return ret
+    pass
