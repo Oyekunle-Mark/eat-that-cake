e7144a1b5e3bdebe78a5f6ed1c2969039a45a497
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-04 19:09:12 +0100

    First pass solution for reverse

--- array_and_string_manipulation/reverse.py
+++ array_and_string_manipulation/reverse.py
@@ -1,2 +1,8 @@
 def reverse(list_of_chars):
+    # set the start_index to zero
+    # set the end_index to length of list_of_chars minus one
+    # loop while start_index is less than end_index
+        # swap values at start_index and end_index
+        # increment start_index
+        # decrement end_index
     pass
