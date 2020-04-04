7e0bbaefb45c861a4aa4812156655852d7f5745d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-04 18:35:49 +0100

    IMplementation of the first pass solution

--- array_and_string_manipulation/merge_ranges.py
+++ array_and_string_manipulation/merge_ranges.py
@@ -1,14 +1,23 @@
 def merge_ranges(meetings):
     # create a new copy of the meetings and sort it in the process
-    # initialize a variable ret to am empty list
+    meetings = sorted(meetings)
+    # initialize a variable ret to an empty list
+    ret = []
     # initialize variables start and end to the intergers
     # in the first tuple in meetings
+    start, end = meetings[0]
     # loop through meetings
+    for index in range(1, len(meetings) - 1):
         # compare end to the start time of current meeting
         # if end is greater than or equal to start time
+        if end >= meetings[index][1]:
             # set end to the end time of current meeting
+            end = meetings[index][1]
         # otherwise, if end is less than start time
+        else:
             # append start time and end to ret as a tuple
+            ret.append((start, end))
             # set start and end to the start and end times of next meeting
+            start, end = meetings[index + 1]
     # return ret
-    pass
+    return ret
