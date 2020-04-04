a2e865a1df9f46c686b0231e1984e8b3d621c0ba
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-04 19:12:26 +0100

    Added a new empty line

--- array_and_string_manipulation/reverse.py
+++ array_and_string_manipulation/reverse.py
@@ -4,6 +4,7 @@ def reverse(list_of_chars):
     # set the end_index to length of list_of_chars minus one
     end_index = len(list_of_chars) - 1
     # loop while start_index is less than end_index
+
     while start_index < end_index:
         # swap values at start_index and end_index
         list_of_chars[start_index], list_of_chars[end_index] = list_of_chars[end_index], list_of_chars[start_index]
