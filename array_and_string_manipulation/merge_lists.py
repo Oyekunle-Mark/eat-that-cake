7d8bce555017eec0fb5dc4f01e833f2d7ebcd9ba
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-05 20:33:53 +0100

    Spaced out the code

--- array_and_string_manipulation/merge_lists.py
+++ array_and_string_manipulation/merge_lists.py
@@ -3,6 +3,7 @@ def merge_lists(my_list, alices_list):
     index_one, index_two = 0, 0
     # initialize merged_list to an empty list
     merged_list = []
+
     # loop while index_one is less than length of my_list
     # and index_two is less than length of alices_list
     while index_one < len(my_list) and index_two < len(alices_list):
@@ -19,18 +20,21 @@ def merge_lists(my_list, alices_list):
             merged_list.append(alices_list[index_two])
             # increment index_two
             index_two += 1
+
     # if index_one is less than length of my_list
     if index_one < len(my_list):
         # append value at index_one of my_list into merged_list
         merged_list.append(my_list[index_one])
         # increment index_one
         index_one += 1
+
     # if index_two is less than length of alices_list
     if index_two < len(alices_list):
         # append value at index_two of alices_list into merged_list
         merged_list.append(alices_list[index_two])
         # increment index_two
         index_two += 1
+
     # return merged_list
     return merged_list
 
