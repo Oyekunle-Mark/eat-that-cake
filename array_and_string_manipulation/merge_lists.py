2c77e82ef991afc7bf8b9ad4cd9862551045afea
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-05 20:28:17 +0100

    Implementation of first pass solution

--- array_and_string_manipulation/merge_lists.py
+++ array_and_string_manipulation/merge_lists.py
@@ -1,20 +1,35 @@
 def merge_lists(my_list, alices_list):
     # initialize index_one and index_two to zero
+    index_one, index_two = 0, 0
     # initialize merged_list to an empty list
+    merged_list = []
     # loop while index_one is less than length of my_list
     # and index_two is less than length of alices_list
+    while index_one < len(my_list) and index_two < len(alices_list):
         # if value at index_one of my_list is less than
         # value at index_two of alices_list
+        if my_list[index_one] < alices_list[index_two]:
             # append value at index_one of my_list into merged_list
+            merged_list.append(my_list[index_one])
             # increment index_one
+            index_one += 1
         # otherwise
+        else:
             # append value at index_two of alices_list into merged_list
+            merged_list.append(alices_list[index_two])
             # increment index_two
+            index_two += 1
     # if index_one is less than length of my_list
+    if index_one < len(my_list):
         # append value at index_one of my_list into merged_list
+        merged_list.append(my_list[index_one])
         # increment index_one
+        index_one += 1
     # if index_two is less than length of alices_list
+    if index_two < len(alices_list):
         # append value at index_two of alices_list into merged_list
+        merged_list.append(alices_list[index_two])
         # increment index_two
+        index_two += 1
     # return merged_list
-    pass
+    return merged_list
