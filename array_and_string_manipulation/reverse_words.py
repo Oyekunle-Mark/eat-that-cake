791bd02a6628019e8c60c0555d4a3ec87e2ef3a0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-05 19:56:23 +0100

    First pass solution

--- array_and_string_manipulation/reverse_words.py
+++ array_and_string_manipulation/reverse_words.py
@@ -1,9 +1,11 @@
 # create a reverse_array_portion which reverses section of a list
 # it should take the start_index, end_index and the list to be reversed
+
+
 def reverse_array_portion(start_index, end_index, arr):
     # loop while start_index is less than end_index
     while start_index < end_index:
-    # swap item at start_index and end_index
+        # swap item at start_index and end_index
         arr[start_index], arr[end_index] = arr[end_index], arr[start_index]
         # increment start_index
         start_index += 1
@@ -13,11 +15,18 @@ def reverse_array_portion(start_index, end_index, arr):
 
 def reverse_words(message):
     # grab the start and end index of message
+    start_index = 0
+    end_index = len(message) - 1
     # reverse the entire list in place
+    reverse_array_portion(start_index, end_index, message)
     # loop through for every character in message
+    for character, current_index in enumerate(message):
         # if current character is an empty space
-            # reverse the characters from start_index to current_index
-            # set start_index to current_index
+        if character == ' ':
+            # reverse the characters from start_index to current_index minus one
+            reverse_array_portion(start_index, current_index - 1, message)
+            # set start_index to current_index plus one
+            start_index = current_index + 1
     # remember to swap the characters of the last word
     # swap using start_index and end_index
-    pass
+    reverse_array_portion(start_index, end_index, message)
