61049fb72905fff9871d20fc7d6b9c7dcd65e8a5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-05 19:46:53 +0100

    Helper function description and pseudocode

--- array_and_string_manipulation/reverse_words.py
+++ array_and_string_manipulation/reverse_words.py
@@ -1,2 +1,12 @@
+# create a reverse_array_portion which reverses section of a list
+# it should take the start_index, end_index and the list to be reversed
+def reverse_array_portion(start_index, end_index, arr):
+    # loop while start_index is less than end_index
+    # swap item at start_index and end_index
+    # increment start_index
+    # decrement end_index
+    pass
+
+
 def reverse_words(message):
     pass
