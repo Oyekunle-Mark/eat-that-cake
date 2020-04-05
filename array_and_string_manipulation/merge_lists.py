3ed2415a07044d38dfc87c3c66c94b06f82928da
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-05 20:25:16 +0100

    First pass solution pseudocode

--- array_and_string_manipulation/merge_lists.py
+++ array_and_string_manipulation/merge_lists.py
@@ -1,2 +1,20 @@
 def merge_lists(my_list, alices_list):
+    # initialize index_one and index_two to zero
+    # initialize merged_list to an empty list
+    # loop while index_one is less than length of my_list
+    # and index_two is less than length of alices_list
+        # if value at index_one of my_list is less than
+        # value at index_two of alices_list
+            # append value at index_one of my_list into merged_list
+            # increment index_one
+        # otherwise
+            # append value at index_two of alices_list into merged_list
+            # increment index_two
+    # if index_one is less than length of my_list
+        # append value at index_one of my_list into merged_list
+        # increment index_one
+    # if index_two is less than length of alices_list
+        # append value at index_two of alices_list into merged_list
+        # increment index_two
+    # return merged_list
     pass
