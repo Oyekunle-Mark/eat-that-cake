41715154a957c417cd71c7db8a4768ff316a11f7
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-05 20:06:00 +0100

    Made the function description  a docstring

--- array_and_string_manipulation/reverse_words.py
+++ array_and_string_manipulation/reverse_words.py
@@ -1,8 +1,8 @@
-# create a reverse_array_portion which reverses section of a list
-# it should take the start_index, end_index and the list to be reversed
-
-
 def reverse_array_portion(start_index, end_index, arr):
+    """
+    create a reverse_array_portion which reverses section of a list
+    it should take the start_index, end_index and the list to be reversed
+    """
     # loop while start_index is less than end_index
     while start_index < end_index:
         # swap item at start_index and end_index
