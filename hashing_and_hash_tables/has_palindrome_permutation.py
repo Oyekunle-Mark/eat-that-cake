fb717b5e37c75483de767157c03d6de6f4ef0867
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-06 21:32:35 +0100

    First pass solution implementation

--- hashing_and_hash_tables/has_palindrome_permutation.py
+++ hashing_and_hash_tables/has_palindrome_permutation.py
@@ -1,15 +1,27 @@
 def has_palindrome_permutation(the_string):
     # initialize character_count to an empty dictionary
+    character_count = {}
     # loop through every character in the_string
+    for character in the_string:
         # if character is in character_count
+        if character in character_count:
             # increment the count
+            character_count[character] += 1
         # otherwise
+        else:
             #  add it to character_count and set it as 1
+            character_count[character] = 1
     # initialize even_count to zero
+    even_count = 0
     # loop through every count in character_count
+    for _, count in character_count.items():
         # if count is not even
+        if count % 2 != 0:
             # if even_count equals one
+            if even_count == 1:
                 # return False
+                return False
             # increment even_count by one
+            even_count += 1
     # return True
-    pass
+    return True
