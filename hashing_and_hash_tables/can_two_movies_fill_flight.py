2ad97d359bcd947f09e307aa9d27b3ced8f030b9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-06 21:12:09 +0100

    First pass solution

--- hashing_and_hash_tables/can_two_movies_fill_flight.py
+++ hashing_and_hash_tables/can_two_movies_fill_flight.py
@@ -1,2 +1,8 @@
 def can_two_movies_fill_flight(movie_lengths, flight_length):
+    # initialize movie_lengths_dict to empty dictionary
+    # loop through movie in movie_lengths
+        # if flight_length minus movie is in movie_lengths_dict
+            # return True
+        # add movie to the movie_lengths_dict and set its value to True
+    # return False
     pass
