5730ae79af77399225129a18053c5c4f72bad5ec
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-06 21:30:11 +0100

    First pass solution pseudocode

--- hashing_and_hash_tables/has_palindrome_permutation.py
+++ hashing_and_hash_tables/has_palindrome_permutation.py
@@ -1,2 +1,15 @@
 def has_palindrome_permutation(the_string):
+    # initialize character_count to an empty dictionary
+    # loop through every character in the_string
+        # if character is in character_count
+            # increment the count
+        # otherwise
+            #  add it to character_count and set it as 1
+    # initialize even_count to zero
+    # loop through every count in character_count
+        # if count is not even
+            # if even_count equals one
+                # return False
+            # increment even_count by one
+    # return True
     pass
