649e1750a60ed63012d379d8798c1154eab1b776
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-06 11:29:26 +0100

    Spaced out the logic

--- array_and_string_manipulation/is_first_come_first_served.py
+++ array_and_string_manipulation/is_first_come_first_served.py
@@ -5,6 +5,7 @@ def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
     # last allowable index in both lists
     take_out_limit = len(take_out_orders) - 1
     dine_in_limit = len(dine_in_orders) - 1
+
     # loop for every order in served_orders
     for order in served_orders:
         # if the take_out_index is less than or equal to take_out_limit
@@ -21,10 +22,12 @@ def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
         else:
             # return False
             return False
+
     # if there is an extra order/orders that has not been served
     if take_out_index != len(take_out_orders) or dine_in_index != len(dine_in_orders):
         # return False
         return False
+
     # return True
     return True
 
