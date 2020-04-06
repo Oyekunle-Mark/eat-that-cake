380c65432466a864d16894da614430bfe6cb2a70
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-06 10:39:24 +0100

    First pass solution pseudocode

--- array_and_string_manipulation/is_first_come_first_served.py
+++ array_and_string_manipulation/is_first_come_first_served.py
@@ -1,2 +1,11 @@
 def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
+    # set take_out_index and dine_in_index to zero
+    # loop for every order in served_orders
+        # if order is take_out_index of take_out_orders
+            # increment take_out_index
+        # otherwise, if order is dine_in_index of dine_in_orders
+            # increment dine_in_index
+        # otherwise
+            # return False
+    # return True
     pass
