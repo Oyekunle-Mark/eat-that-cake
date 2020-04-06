d9c81967f6c9cfc3aaa0ab561019e3dab01669d9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-06 10:47:34 +0100

    Caught the index out of bounds error

--- array_and_string_manipulation/is_first_come_first_served.py
+++ array_and_string_manipulation/is_first_come_first_served.py
@@ -1,16 +1,24 @@
 def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
     # set take_out_index and dine_in_index to zero
     take_out_index, dine_in_index = 0, 0
+    # initialize take_out_limit and dine_in_limit to the
+    # last allowable index in both lists
+    take_out_limit = len(take_out_orders) - 1
+    dine_in_limit = len(dine_in_orders) - 1
     # loop for every order in served_orders
     for order in served_orders:
         # if order is take_out_index of take_out_orders
         if order == take_out_orders[take_out_index]:
-            # increment take_out_index
-            take_out_index += 1
+            # if take_out_index is less than take_out_limit
+            if take_out_index < take_out_limit:
+                # increment take_out_index
+                take_out_index += 1
         # otherwise, if order is dine_in_index of dine_in_orders
         elif order == dine_in_orders[dine_in_index]:
-            # increment dine_in_index
-            dine_in_index += 1
+            # if dine_in_index is less than dine_in_limit
+            if dine_in_index < dine_in_limit:
+                # increment dine_in_index
+                dine_in_index += 1
         # otherwise
         else:
             # return False
