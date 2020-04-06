582e831fd6ffaf6b570f25d2bb1559912184b890
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-06 11:26:26 +0100

    Fixed the logic

--- array_and_string_manipulation/is_first_come_first_served.py
+++ array_and_string_manipulation/is_first_come_first_served.py
@@ -9,22 +9,24 @@ def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
     for order in served_orders:
         # if take_out_orders is not an empty list
         # and order is take_out_index of take_out_orders
-        if take_out_orders and order == take_out_orders[take_out_index]:
-            # if take_out_index is less than take_out_limit
-            if take_out_index < take_out_limit:
-                # increment take_out_index
-                take_out_index += 1
+        if take_out_index <= take_out_limit and order == take_out_orders[take_out_index]:
+            
+            # increment take_out_index
+            take_out_index += 1
         # otherwise, if dine_in_orders is not an empty list
         # and order is dine_in_index of dine_in_orders
-        elif dine_in_orders and order == dine_in_orders[dine_in_index]:
-            # if dine_in_index is less than dine_in_limit
-            if dine_in_index < dine_in_limit:
-                # increment dine_in_index
-                dine_in_index += 1
+        elif dine_in_index <= dine_in_limit and order == dine_in_orders[dine_in_index]:
+            
+            # increment dine_in_index
+            dine_in_index += 1
         # otherwise
         else:
             # return False
             return False
+    # if there is an extra order/orders that has not been served
+    if take_out_index != len(take_out_orders) or dine_in_index != len(dine_in_orders):
+        # return False
+        return False
     # return True
     return True
 
@@ -32,3 +34,5 @@ def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
 print(is_first_come_first_served([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6]))
 print(is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 6, 3, 5]))
 print(is_first_come_first_served([], [2, 3, 6], [2, 3, 6]))
+print(is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8]))
+print(is_first_come_first_served([1, 9], [7, 8], [1, 7, 8]))
