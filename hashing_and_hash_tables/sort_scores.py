1e7c514c800a55774670c18ed40561300ad718bd
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-07 13:26:24 +0100

    Fixed the off by one error

--- hashing_and_hash_tables/sort_scores.py
+++ hashing_and_hash_tables/sort_scores.py
@@ -16,12 +16,13 @@ def sort_scores(unsorted_scores, highest_possible_score):
     # loop through every index of score_count backwards
     for index in range(len(score_count), -1, -1):
         # if score_count[index] is not None:
-        if score_count[index] is not None:
+        if score_count[index - 1] is not None:
             # append index to sorted_scores score_count[index] number of times
-            for _ in range(score_count[index]):
-                sorted_scores.append(index)
+            for _ in range(score_count[index - 1]):
+                sorted_scores.append(index - 1)
     # return sorted_scores
     return sorted_scores
 
 
 print(sort_scores([37, 89, 41, 65, 91, 53], 100))
+print(sort_scores([20, 10, 30, 30, 10, 20], 100))
