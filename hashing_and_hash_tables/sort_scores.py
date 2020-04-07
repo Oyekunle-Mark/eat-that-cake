70a15c3ec02294827f7bf6dc8d3f98fad896c5bc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-07 13:04:18 +0100

    First pass solution pseudcode

--- hashing_and_hash_tables/sort_scores.py
+++ hashing_and_hash_tables/sort_scores.py
@@ -1,2 +1,13 @@
 def sort_scores(unsorted_scores, highest_possible_score):
+    # initialize a new list score_count to the size of highest_possible_score plus one
+    # initialize sorted_scores to an empty list
+    # loop through every score in unsorted_scores
+        # if item at score index of score_count is not None
+            # increment the item
+        # otherwise,
+            # set the score index of score_count to one
+    # loop through every count in score_count:
+        # if current count is not None
+            # append the current index to sorted_scores count number of times
+    # return sorted_scores
     pass
