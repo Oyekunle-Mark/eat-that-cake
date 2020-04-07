7e3e48bda4324034c0d1cf10c323b15979a9309c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-07 10:06:28 +0100

    Pseudocode for the first pass split_word function

--- hashing_and_hash_tables/word_cloud_data.py
+++ hashing_and_hash_tables/word_cloud_data.py
@@ -3,4 +3,16 @@ def word_cloud_data(the_string):
 
 
 def split_word(string):
+    # initialize split_words to an empty list
+    # initialize current_word to en empty string
+    # loop through string
+        # if character is an alphabet or an apostrophe
+            # append character to current_word
+        # if character is is a hyphen and current_word is not empty
+            # append character to current_word
+        # otherwise, if current_word is empty
+            # continue to the next character
+        # otherwise
+            # append current_word to split_words
+    # return split_words
     pass
