be726f6a96541e5502b3a13f3296a22996b2003d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-07 10:34:16 +0100

    Spaced out blocks of codes

--- hashing_and_hash_tables/word_cloud_data.py
+++ hashing_and_hash_tables/word_cloud_data.py
@@ -3,6 +3,7 @@ def word_cloud_data(the_string):
     split_string = split_word(the_string)
     # initialize word_count to empty dictionary
     word_count = {}
+
     # loop through for every word in split_string
     for word in split_string:
         # if word to lower case is in word_count
@@ -14,6 +15,7 @@ def word_cloud_data(the_string):
             # add the word to lower case to the word_count
             # and set the value to one
             word_count[word.lower()] = 1
+
     # return word_count
     return word_count
 
@@ -23,6 +25,7 @@ def split_word(string):
     split_words = []
     # initialize current_word to en empty string
     current_word = ''
+
     # loop through string
     for character in string:
         # if character is an alphabet or an apostrophe
@@ -43,11 +46,13 @@ def split_word(string):
             split_words.append(current_word)
             # reset current_word
             current_word = ''
+
     # append the last word
     # if current_word
     if current_word:
         # append to split_words
         split_words.append(current_word)
+
     # return split_words
     return split_words
 
