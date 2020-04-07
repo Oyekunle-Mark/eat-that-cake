eeaa7f00e8d3aa5908b280cdad32c949978ed0f8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-07 10:23:10 +0100

    Implemented the first pass solution

--- hashing_and_hash_tables/word_cloud_data.py
+++ hashing_and_hash_tables/word_cloud_data.py
@@ -1,14 +1,21 @@
 def word_cloud_data(the_string):
     # initialize split_string to the return value of split_word
+    split_string = split_word(the_string)
     # initialize word_count to empty dictionary
+    word_count = {}
     # loop through for every word in split_string
+    for word in split_string:
         # if word to lower case is in word_count
+        if word.lower() in word_count:
             # increment the count
+            word_count[word.lower()] += 1
         # otherwise
+        else:
             # add the word to lower case to the word_count
             # and set the value to one
+            word_count[word.lower()] = 1
     # return word_count
-    pass
+    return word_count
 
 
 def split_word(string):
