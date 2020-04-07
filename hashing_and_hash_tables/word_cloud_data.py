171352ea11d864079aa06ba8e87ae4a3424cc89c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-07 10:12:41 +0100

    First pass solution for the split_word function

--- hashing_and_hash_tables/word_cloud_data.py
+++ hashing_and_hash_tables/word_cloud_data.py
@@ -4,18 +4,29 @@ def word_cloud_data(the_string):
 
 def split_word(string):
     # initialize split_words to an empty list
+    split_words = []
     # initialize current_word to en empty string
+    current_word = ''
     # loop through string
+    for character in string:
         # if character is an alphabet or an apostrophe
+        if character.isalpha() or character == "'":
             # append character to current_word
-        # if character is is a hyphen and current_word is not empty
+            current_word += character
+        # if character is a hyphen and current_word is not empty
+        elif character == '-' and current_word != '':
             # append character to current_word
+            current_word += character
         # otherwise, if current_word is empty
+        elif current_word == '':
             # continue to the next character
+            continue
         # otherwise
+        else:
             # append current_word to split_words
+            split_words.append(current_word)
     # return split_words
-    pass
+    return split_words
 
 
 print(word_cloud_data('Chocolate cake for dinner and pound cake for dessert'))
