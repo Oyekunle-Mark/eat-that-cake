c68727fb7208ffd8dd97c046eff42a4389c6b835
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-09 18:09:46 +0100

    Pseudocode for the third pass solution

--- greedy_algorithms/highest_product_of_3.py
+++ greedy_algorithms/highest_product_of_3.py
@@ -1,2 +1,22 @@
 def highest_product_of_3(list_of_ints):
+    # initialize lowest to the minimum of the first two integers
+    # initialize highest to the maximum of the first two integers
+    # initialize lowest_product_of_two and highest_product_of_two
+    # to the product of the first two integers
+    # initialize highest_product_of_three to the product of the first three integers
+    # loop through every integer in list_of_ints
+    # starting from the third
+        # set integer to the value at index of list_of_ints
+        # set highest_product_of_three to the maximum of highest_product_of_three
+        # integer multiplied by highest_product_of_two and
+        # integer multiplied by lowest_product_of_two
+        
+        # set highest_product_of_two to the maximum of of highest_product_of_two
+        # and interger multiplied by lowest and integer multiplied by highest
+
+        # set lowest_product_of_two to the minimum of lowest_product_of_two
+        # and integer multiplied by lowest and integer multiplied by highest
+
+        # set lowest to the minimum of lowest and interger
+        # set highest to the maximum of highest and interger
     pass
