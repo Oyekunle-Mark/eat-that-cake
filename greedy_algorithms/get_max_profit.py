505a4280c77c65b89d3b24157ef63b9475020a70
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-09 15:52:12 +0100

    Formatted the code

--- greedy_algorithms/get_max_profit.py
+++ greedy_algorithms/get_max_profit.py
@@ -4,6 +4,7 @@ def get_max_profit(stock_prices):
     # initialize the max_profit to the
     # difference between the first and the second stock price
     max_profit = stock_prices[1] - stock_prices[0]
+
     # loop through every price in stock_prices
     # starting from the second price
     for index in range(1, len(stock_prices)):
@@ -11,9 +12,11 @@ def get_max_profit(stock_prices):
         if stock_prices[index] - lowest_price > max_profit:
             # set max_profit to the difference between price and lowest_price
             max_profit = stock_prices[index] - lowest_price
+
         # if price is lower than lowest_price
         if stock_prices[index] < lowest_price:
             # set lowest_price to price
             lowest_price = stock_prices[index]
+
     # return max_profit
     return max_profit
