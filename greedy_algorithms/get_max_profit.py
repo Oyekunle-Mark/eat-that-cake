4fb4b608d40dbff4cee2f7cf63f7810d0790dce2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-09 15:39:13 +0100

    First pass pseudocode

--- greedy_algorithms/get_max_profit.py
+++ greedy_algorithms/get_max_profit.py
@@ -1,2 +1,11 @@
 def get_max_profit(stock_prices):
+    # initialize the lowest_price to the first stock price
+    # initialize the max_profit to the difference between the first and the second stock price
+    # loop through every price in stock_prices
+    # starting from the second price
+        # if price minus lowest_price is less than max_profit
+            # set max_profit to the difference between price and lowest_price
+        # if price is lower than lowest_price
+            # set lowest_price to price
+    # return max_profit
     pass
