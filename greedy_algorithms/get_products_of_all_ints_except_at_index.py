f62b45319dde9a3db99755ce8d3a42ca552341a3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-12 14:50:29 +0100

    Pseudocode for first pass algorithm

--- greedy_algorithms/get_products_of_all_ints_except_at_index.py
+++ greedy_algorithms/get_products_of_all_ints_except_at_index.py
@@ -1,2 +1,17 @@
 def get_products_of_all_ints_except_at_index(int_list):
+    # initialize product_at_all_ints_except_index to list
+    # of Nones of size int_list
+    # set product_so_far to one
+    # loop through every int in int_list
+        # set value at current index of product_at_all_ints_except_index
+        # to the product_so_far
+        # set product_so_far to the product of product_so_far and
+        # integer at current index of int_list
+    # set product_so_far to one
+    # loop through int_list backwards
+    # set value at current index of product_at_all_ints_except_index to the
+    # product of product_so_far and value at current index of product_at_all_ints_except_index
+    # set product_so_far to the product of product_so_far and
+    # integer at current index of int_list
+    # return product_at_all_ints_except_index
     pass
