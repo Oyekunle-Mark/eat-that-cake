015d44e2d8d301cf3d7bc396b8fad484707b8665
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-12 14:53:32 +0100

    First pass solution implementation

--- greedy_algorithms/get_products_of_all_ints_except_at_index.py
+++ greedy_algorithms/get_products_of_all_ints_except_at_index.py
@@ -1,17 +1,26 @@
 def get_products_of_all_ints_except_at_index(int_list):
     # initialize product_at_all_ints_except_index to list
     # of Nones of size int_list
+    product_at_all_ints_except_index = [None] * len(int_list)
     # set product_so_far to one
+    product_so_far = 1
     # loop through every int in int_list
+    for index in range(0, len(int_list)):
         # set value at current index of product_at_all_ints_except_index
         # to the product_so_far
+        product_at_all_ints_except_index[index] = product_so_far
         # set product_so_far to the product of product_so_far and
         # integer at current index of int_list
+        product_so_far *= int_list[index]
     # set product_so_far to one
+    product_so_far = 1
     # loop through int_list backwards
-    # set value at current index of product_at_all_ints_except_index to the
-    # product of product_so_far and value at current index of product_at_all_ints_except_index
-    # set product_so_far to the product of product_so_far and
-    # integer at current index of int_list
+    for index in range(len(int_list) - 1, -1, -1):
+        # set value at current index of product_at_all_ints_except_index to the
+        # product of product_so_far and value at current index of product_at_all_ints_except_index
+        product_at_all_ints_except_index[index] *= product_so_far
+        # set product_so_far to the product of product_so_far and
+        # integer at current index of int_list
+        product_so_far *= int_list[index]
     # return product_at_all_ints_except_index
-    pass
+    return product_at_all_ints_except_index
