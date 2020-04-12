966eff1b4b009b5c3989a519763c697fd13cc8c4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-12 15:25:09 +0100

    Implementation of the first pass

--- greedy_algorithms/shuffle.py
+++ greedy_algorithms/shuffle.py
@@ -7,8 +7,11 @@ def get_random(floor, ceiling):
 
 def shuffle(the_list):
     # initialize ceiling to the length of the_list minus one
+    ceiling = len(the_list) - 1
     # loop through every index in the_list
+    for index in range(0, len(the_list)):
         # initialize random_index to the return value of
         # get_random passing in index and ceiling
+        random_index = get_random(index, ceiling)
         # swap items at random_index and index
-    pass
+        the_list[index], the_list[random_index] = the_list[random_index], the_list[index]
