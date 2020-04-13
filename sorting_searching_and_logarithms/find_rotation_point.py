c337e1c2069d00031137e428e31701aa4b767507
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-13 09:58:46 +0100

    Fixed the comments

--- sorting_searching_and_logarithms/find_rotation_point.py
+++ sorting_searching_and_logarithms/find_rotation_point.py
@@ -3,13 +3,13 @@ def find_rotation_point(words):
     first_word = words[0]
     # initialize floor_index to zero
     floor_index = 0
-    # initialize ceiling_index to zero
+    # initialize ceiling_index to the index of last word in words
     ceiling_index = len(words) - 1
     # loop while floor_index is less than ceiling_index
     while floor_index < ceiling_index:
         # set midpoint to the average of floor_index and ceiling_index
         midpoint = (floor_index + ceiling_index) // 2
-        # if midpoint is greater than first_word
+        # if word at midpoint of words is greater than or equals first_word
         if words[midpoint] >= first_word:
             # set floor_index to midpoint
             floor_index = midpoint
@@ -17,7 +17,6 @@ def find_rotation_point(words):
         else:
             # set ceiling_index to midpoint
             ceiling_index = midpoint
-
         # If floor and ceiling have converged
         if floor_index + 1 == ceiling_index:
             # Between floor and ceiling is where we flipped to the beginning
