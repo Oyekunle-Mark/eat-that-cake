fa0f08faffe3404a3b37fb3918177abbfdb9fdc1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-13 09:47:58 +0100

    Pseudocodefor first pass solution

--- sorting_searching_and_logarithms/find_rotation_point.py
+++ sorting_searching_and_logarithms/find_rotation_point.py
@@ -1,2 +1,12 @@
 def find_rotation_point(words):
+    # initialize first_word to the first_item in words
+    # initialize floor_index to zero
+    # initialize ceiling_index to zero
+    # loop while floor_index is less than ceiling_index
+        # set midpoint to the average of floor_index and ceiling_index
+        # if midpoint is greater than first_word
+            # set floor_index to midpoint
+        # otherwise
+            # set ceiling_index to midpoint
+    # return ceiling_index
     pass
