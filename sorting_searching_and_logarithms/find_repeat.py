c2e29b5d9812a43f2c138060b05b96822780aaf8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-14 18:28:49 +0100

    Separated blocks of code

--- sorting_searching_and_logarithms/find_repeat.py
+++ sorting_searching_and_logarithms/find_repeat.py
@@ -3,6 +3,7 @@ def find_repeat(numbers):
     numbers.sort()
     # initialize a variable previous_number to zero
     previous_number = 0
+
     # loop through every number in numbers
     for number in numbers:
         # if current number equals previous_number
