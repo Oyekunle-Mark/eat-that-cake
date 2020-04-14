8f82ad8953a26ced174eca588682164057448240
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-14 18:24:57 +0100

    First pass solution pseudocode

--- sorting_searching_and_logarithms/find_repeat.py
+++ sorting_searching_and_logarithms/find_repeat.py
@@ -1,2 +1,8 @@
 def find_repeat(numbers):
+    # sort numbers in place
+    # initialize a variable previous_number to zero
+    # loop through every number in numbers
+        # if current number equals previous_number
+            # return previous_number
+        # set previous_number to the current number
     pass
