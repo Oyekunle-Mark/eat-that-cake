0fda6188d1eb213e2aceccbc2a8f4ec831c749a5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-14 18:26:21 +0100

    Implementation of the first pass solution

--- sorting_searching_and_logarithms/find_repeat.py
+++ sorting_searching_and_logarithms/find_repeat.py
@@ -1,8 +1,13 @@
 def find_repeat(numbers):
     # sort numbers in place
+    numbers.sort()
     # initialize a variable previous_number to zero
+    previous_number = 0
     # loop through every number in numbers
+    for number in numbers:
         # if current number equals previous_number
+        if number == previous_number:
             # return previous_number
+            return previous_number
         # set previous_number to the current number
-    pass
+        previous_number = number
