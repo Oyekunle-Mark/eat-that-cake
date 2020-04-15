fea086bff92f8e52c9da2eafcce802cfa6258edd
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-15 13:35:54 +0100

    Fixed typos

--- trees_and_graphs/is_balanced.py
+++ trees_and_graphs/is_balanced.py
@@ -4,7 +4,7 @@ def is_balanced(tree_root):
     # initialize depths to empty list
     depths = []
     # push node and depth as a zero onto stack as a tuple
-    stack.push((tree_root, 0))
+    stack.append((tree_root, 0))
     # while stack is not empty
     while len(stack):
         # pop the node and depth from stack
@@ -13,19 +13,19 @@ def is_balanced(tree_root):
         # that is, the node is a leaf node
         if node.left is None and node.right is None:
             # push depth onto the depths list
-            depths.push(depth)
+            depths.append(depth)
             # if depths is longer that two or absolute difference between
             # the two depths is greater than one
-            if len(depths) > 2 or abs(depths[0] - depths[1]) > 1:
+            if len(depths) > 2 or (len(depths) > 1 and abs(depths[0] - depths[1]) > 1):
                 # return False
                 return False
         # if there is a left child
         if node.left is not None:
             # push the left node and its depth plus one onto the stack
-            stack.push((node.left, 1 + depth))
+            stack.append((node.left, 1 + depth))
         # if there is a right child
         if node.right is not None:
             # push the right node and its depth plus one onto the stack
-            stack.push((node.right, 1 + depth))
+            stack.append((node.right, 1 + depth))
     # return True
     return True
