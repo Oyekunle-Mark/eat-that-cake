78efbe0288361ddd82980033eaebb6ca1a795ee1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-16 09:03:01 +0100

    First pass pseudocode

--- trees_and_graphs/is_binary_search_tree.py
+++ trees_and_graphs/is_binary_search_tree.py
@@ -17,4 +17,19 @@ class BinaryTreeNode(object):
 
 
 def is_binary_search_tree(root):
+    # check if the root is an empty node
+        # return True
+    # create a stack
+    # push root onto the stack
+    # loop while stack is not empty
+        # pop node from the stack
+        # if node has a left child
+            # if value of left child is greater than value of node
+                # return False
+            # push node onto the stack
+        # if node has a right child
+            # if value of right child is greater than value of node
+                # return False
+            # push node onto the stack
+    # return True
     pass
