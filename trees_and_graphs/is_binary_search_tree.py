4be3666f76cf5934c08f1a39ec00f7505f271e48
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-16 08:55:51 +0100

    sample binary search tree node class

--- trees_and_graphs/is_binary_search_tree.py
+++ trees_and_graphs/is_binary_search_tree.py
@@ -1,2 +1,20 @@
+"""
+class BinaryTreeNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.left  = None
+        self.right = None
+
+    def insert_left(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insert_right(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
+"""
+
+
 def is_binary_search_tree(root):
     pass
