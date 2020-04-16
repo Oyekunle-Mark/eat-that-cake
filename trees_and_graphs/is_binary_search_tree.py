f853cd726250ff6318918c5e58ff5b2e8df09f6e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-16 09:06:15 +0100

    First pass implementation

--- trees_and_graphs/is_binary_search_tree.py
+++ trees_and_graphs/is_binary_search_tree.py
@@ -18,18 +18,32 @@ class BinaryTreeNode(object):
 
 def is_binary_search_tree(root):
     # check if the root is an empty node
+    if root.value is None:
         # return True
+        return True
     # create a stack
+    stack = []
     # push root onto the stack
+    stack.append(root)
     # loop while stack is not empty
+    while len(stack) > 0:
         # pop node from the stack
+        node = stack.pop()
         # if node has a left child
+        if node.left is not None:
             # if value of left child is greater than value of node
+            if node.left.value > node.value:
                 # return False
+                return False
             # push node onto the stack
+            stack.append(node.left)
         # if node has a right child
-            # if value of right child is greater than value of node
+        if node.right is not None:
+            # if value of right child is less than value of node
+            if node.right.value < node.value:
                 # return False
+                return False
             # push node onto the stack
+            stack.append(node.right)
     # return True
-    pass
+    return True
