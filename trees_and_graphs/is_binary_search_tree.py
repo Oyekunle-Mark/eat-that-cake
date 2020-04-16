abd5c501b778e5dd3b2ecd600e5ff70e662606d9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-16 09:43:33 +0100

    Psuedocode of second pass solution

--- trees_and_graphs/is_binary_search_tree.py
+++ trees_and_graphs/is_binary_search_tree.py
@@ -17,4 +17,21 @@ class BinaryTreeNode(object):
 
 
 def is_binary_search_tree(root):
+    # if the root node is empty
+        # return True
+    # create a stack to hold the at tuple of the node, the lower_bound and upper_bound
+    # push the root, negative infinity as lower bound
+    # and positive infinity as upper bound
+    # loop while stack is not empty
+        # pop the node, lower_bound and upper_bound from the stack
+        # check if node's value is less than or equal to the lower bound
+        # or greater than or equal to the upper bound
+            # return False
+        # if there is a left child
+            # push it onto the stack with negative infinity as lower bound
+            # and node value as upper bound
+        # if there is a right child
+            # push it onto the stack with positive infinity as upper bound
+            # and node value as lower bound
+    # return True
     pass
