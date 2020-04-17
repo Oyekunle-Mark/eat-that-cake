6e2fe2d8e577fc2c720623449c56eb82b58c9d22
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-17 19:51:27 +0100

    Implemented the first pass

--- trees_and_graphs/color_graph.py
+++ trees_and_graphs/color_graph.py
@@ -28,14 +28,23 @@ def color_graph(graph, colors):
     # color to the graph node.
 
     # loop through every graph_node in graph
+    for graph_node in graph:
         # check if the graph node has a loop
         # if graph_node in graph_node.neighbors
+        if graph_node in graph_node.neighbors:
             # raise exception that legal coloring is impossible with a node with a loop
+            raise Exception('Legal coloring impossible for node with loop: %s' %
+                            graph_node.label)
         # get all the colors of neighboring nodes
         # initialize neighbors_colors to a set from the list comprehension
         # of all the neighbor in graph_node neighbors that has a color
+        neighbors_colors = set(
+            [neighbor.color for neighbor in graph_node.neighbors if neighbor.color])
         # loop through every color in colors
+        for color in colors:
             # if color is not in neighbors_colors
+            if color not in neighbors_colors:
                 # set the graph_node color to color
+                graph_node.color = color
                 # break the loop
-    pass
+                break
