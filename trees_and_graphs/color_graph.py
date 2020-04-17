3ef7b4dc71eea63a2a00c3f24975d20323262d8c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-17 19:36:50 +0100

    The graphnode class and graph construction logic

--- trees_and_graphs/color_graph.py
+++ trees_and_graphs/color_graph.py
@@ -1,2 +1,24 @@
+"""
+class GraphNode:
+
+    def __init__(self, label):
+        self.label = label
+        self.neighbors = set()
+        self.color = None
+
+
+a = GraphNode('a')
+b = GraphNode('b')
+c = GraphNode('c')
+
+a.neighbors.add(b)
+b.neighbors.add(a)
+b.neighbors.add(c)
+c.neighbors.add(b)
+
+graph = [a, b, c]
+"""
+
+
 def color_graph(graph, colors):
     pass
