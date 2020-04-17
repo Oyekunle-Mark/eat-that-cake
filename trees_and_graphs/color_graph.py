379281e58d91f39cd2cd98fc37f6f1f10122802f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-17 19:40:57 +0100

    The high level how

--- trees_and_graphs/color_graph.py
+++ trees_and_graphs/color_graph.py
@@ -21,4 +21,9 @@ graph = [a, b, c]
 
 
 def color_graph(graph, colors):
+    # the graph coloring will be done by greedily walking though
+    # the list of graph nodes. At each graph node,
+    # we will get all the colors of the neighboring nodes in a set
+    # and assign the first color from colors that is not in neighborring
+    # color to the graph node.
     pass
