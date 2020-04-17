d723ca05e9ee610ed0bf1ee340134f765f3f4362
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-17 17:54:26 +0100

    Pseudocode for first pass solution

--- trees_and_graphs/find_second_largest.py
+++ trees_and_graphs/find_second_largest.py
@@ -21,4 +21,15 @@ def find_second_largest(root_node):
     # keep going down to the right child node until
     # there is no right child node anymore
     # at that point return the last node parent node
+
+    # if the root_node is empty or does not have any child nodes
+        # raise exception
+    # create a node_stack as an empty list
+    # push the root node onto the stack
+    # loop while stack is not empty
+        # pop from the stack
+        # if the node has a right child node
+            # push the right child node onto the stack
+        # otherwise
+            # return the node's value
     pass
