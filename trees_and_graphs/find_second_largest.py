1d72766c088d306cc6545b4ef15e575ed243a8f1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-17 17:46:56 +0100

    Sample bst node class

--- trees_and_graphs/find_second_largest.py
+++ trees_and_graphs/find_second_largest.py
@@ -1,2 +1,20 @@
+"""
+class BinaryTreeNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.left  = None
+        self.right = None
+
+    def insert_left(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insert_right(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
+"""
+
+
 def find_second_largest(root_node):
     pass
