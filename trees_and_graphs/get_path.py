5e58e4b60337b7dc32ee84e199a87f3562a6dd72
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-20 11:40:44 +0100

    Renamed a variable

--- trees_and_graphs/get_path.py
+++ trees_and_graphs/get_path.py
@@ -4,15 +4,17 @@ from collections import deque
 
 def get_path(graph, start_node, end_node):
     # initialize queue to an instance of deque class
-    # initialize visited to an empty set
+    queue = deque()
+    # initialize visited_node to an empty set
+    visited_nodes = set()
     # enqueue the start_node node as a list into the queue
     # loop while queue is not empty
         # dequeue the path from the queue
         # get the last_node from the path
         # if the last_node is equivalent to the end_node
             # return the path
-        # if the last_node has not been visited
-            # add it to the visited
+        # if the last_node has not been visited_node
+            # add it to the visited_node
             # for every node connected to last_node
                 # initialize new_path to a copy of path
                 # append node to new_path
