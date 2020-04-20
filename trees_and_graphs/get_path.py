6dd2afb73d6fc0d678057190a98b58707e2c35e0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-20 11:40:03 +0100

    Pseudocode for the first pass

--- trees_and_graphs/get_path.py
+++ trees_and_graphs/get_path.py
@@ -1,6 +1,20 @@
 # use the deque class from the collections module
 from collections import deque
 
-def get_path(graph, start_node, end_node):
 
+def get_path(graph, start_node, end_node):
+    # initialize queue to an instance of deque class
+    # initialize visited to an empty set
+    # enqueue the start_node node as a list into the queue
+    # loop while queue is not empty
+        # dequeue the path from the queue
+        # get the last_node from the path
+        # if the last_node is equivalent to the end_node
+            # return the path
+        # if the last_node has not been visited
+            # add it to the visited
+            # for every node connected to last_node
+                # initialize new_path to a copy of path
+                # append node to new_path
+                # enqueue new_path into the queue
     pass
