c8094c77a716fc8c1a9fa72bdb56c309d27af9a4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-20 11:57:27 +0100

    Spaced out the block of codes

--- trees_and_graphs/get_path.py
+++ trees_and_graphs/get_path.py
@@ -7,26 +7,31 @@ def get_path(graph, start_node, end_node):
     if start_node not in graph or end_node not in graph:
         # raise an exception
         raise Exception("Start and End nodes must be in the same graph")
+
     # initialize queue to an instance of deque class
     queue = deque()
     # initialize visited_node to an empty set
     visited_nodes = set()
     # enqueue the start_node node as a list into the queue
     queue.appendleft([start_node])
+
     # loop while queue is not empty
     while len(queue):
         # dequeue the path from the queue
         path = queue.pop()
         # get the last_node from the path
         last_node = path[-1]
+
         # if the last_node is equivalent to the end_node
         if last_node == end_node:
             # return the path
             return path
+
         # if the last_node has not been visited_node
         if last_node not in visited_nodes:
             # add it to the visited_node
             visited_nodes.add(last_node)
+
             # for every node connected to last_node
             for node in graph[last_node]:
                 # initialize new_path to a copy of path
