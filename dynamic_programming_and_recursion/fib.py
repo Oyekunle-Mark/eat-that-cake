116b279d3b67d3222e235923d164b65cbfa51e6e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-22 12:58:33 +0100

    First pass pseudocode

--- dynamic_programming_and_recursion/fib.py
+++ dynamic_programming_and_recursion/fib.py
@@ -1,2 +1,10 @@
 def fib(n):
+    # FIRST PASS => NO Memoization
+
+    # write a base of case of n being zero or one
+    # check if n is zero or one
+        # return n in that case
+
+    # otherwise, return the sum of recursive call
+    # to fib passing in n minus one and n minus two
     pass
