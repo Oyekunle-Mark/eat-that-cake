052ac946c8f3ebbf9d91d5167db6c3e0e921719e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-22 12:59:41 +0100

    Firs pass implementation

--- dynamic_programming_and_recursion/fib.py
+++ dynamic_programming_and_recursion/fib.py
@@ -3,8 +3,10 @@ def fib(n):
 
     # write a base of case of n being zero or one
     # check if n is zero or one
+    if n == 0 or n == 1:
         # return n in that case
+        return n
 
     # otherwise, return the sum of recursive call
     # to fib passing in n minus one and n minus two
-    pass
+    return fib(n - 1) + fib(n - 2)
