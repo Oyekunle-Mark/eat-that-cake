6014b59bf5033eb8e8b422aa3c0bd713d7dcb282
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-22 12:11:53 +0100

    The what

--- dynamic_programming_and_recursion/get_permutations.py
+++ dynamic_programming_and_recursion/get_permutations.py
@@ -1,2 +1,9 @@
 def get_permutations(string):
+    # To find the possible permutations of a string
+    # find the permutations of every character in the
+    # string without the last character, then insert the last
+    # character in each index in the generated permutations
+
+    # generating the possible combinations can be done recursively
+    # with a base case being when input string is of length 1
     pass
