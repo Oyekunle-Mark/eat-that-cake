155555dc77547496a2b9d3715f46dc97b9fe16ec
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-22 13:29:49 +0100

    Third pass solution implemented

--- dynamic_programming_and_recursion/fib.py
+++ dynamic_programming_and_recursion/fib.py
@@ -30,13 +30,24 @@ def fib(n, cache={}):
 def fib_iter(n):
     # write a base of case of n being zero or one
     # check if n is zero or one
+    if n == 0 or n == 1:
         # return n in that case
+        return n
+
     # initialize prev_prev to zero
+    prev_prev = 0
     # initialize prev to one
+    prev = 1
+
     # loop through range of n minus one
+    for _ in range(n - 1):
         # To get nth fibonacci ... do n-1 iterations.
         # initialize current to the sum of prev and prev_prev
+        current = prev + prev_prev
         # initialize prev_prev to prev
+        prev_prev = prev
         # initialize prev to current
+        prev = current
+
     # return current
-    pass
+    return current
