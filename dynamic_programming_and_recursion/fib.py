7da544db8c94b536d5d9d6cfdeff748d4c538f97
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-22 13:03:57 +0100

    Second pass pseudocode

--- dynamic_programming_and_recursion/fib.py
+++ dynamic_programming_and_recursion/fib.py
@@ -1,5 +1,8 @@
+# modify the function signature to take a default parameter cache
+# which defaults to an empty dict
 def fib(n):
     # FIRST PASS => NO Memoization
+    # SECOND PASS => With Memoization
 
     # write a base of case of n being zero or one
     # check if n is zero or one
@@ -7,6 +10,14 @@ def fib(n):
         # return n in that case
         return n
 
+    # check if n is in cache
+        # return value of key n in cache
+
     # otherwise, return the sum of recursive call
     # to fib passing in n minus one and n minus two
     return fib(n - 1) + fib(n - 2)
+
+    # initialize nth_fib to the return value recursive call 
+    # to fib passing in n minus one and n minus two
+    # populate the cache with a key value pair of n and nth_fib
+    # return nth_fib
