5a7def0bc74cfbc3279be6541975e58036e4fc54
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-24 11:15:43 +0100

    Implemented the first pass solution

--- dynamic_programming_and_recursion/change_possibilities.py
+++ dynamic_programming_and_recursion/change_possibilities.py
@@ -1,22 +1,34 @@
 # modify the function signature to take a default parameter
 # current_index which is set to one
-def change_possibilities(amount, denominations):
+def change_possibilities(amount, denominations, current_index=1):
     # To solve this problem, think of it as a sum of subproblems
     # of the possibility of making change for each of the denominations
 
     # check if amount is zero
+    if amount == 0:
         # return one
+        return 1
     # check if amount is less than zero
+    if amount < 0:
         # return zero
+        return 0
     # check if current_index has overshot the denominations
     # if current_index is equivalent to length of denominations
+    if current_index == len(denominations):
         # return zero
+        return 0
     # initialize current_coin to the coin at current_index
     # of denominations
+    current_coin = denominations[current_index]
     # initialize sum_of_change_possibility to zero
+    sum_of_change_possibility = 0
     # loop while amount is greater than or equal to zero
+    while amount >= 0:
         # add the return value of recursive call to change_possibilities
         # passing in the amount, denominations and current_index plus one
+        sum_of_change_possibility += change_possibilities(
+            amount, denominations, current_index + 1)
         # decrement amount by substracting current_coin
+        amount -= current_coin
     # return sum_of_change_possibility
-    pass
+    return sum_of_change_possibility
