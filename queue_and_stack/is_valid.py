8ec690525f791804a67c9bb6357ea1f2ffe56103
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-25 14:45:35 +0100

    First pass solution

--- queue_and_stack/is_valid.py
+++ queue_and_stack/is_valid.py
@@ -27,15 +27,30 @@ class Stack(object):
 def is_valid(code):
     # create a dict and map each opener to a closer
     # and name it openers_to_closers
+    openers_to_closers = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+    }
     # create a set of the openers
+    openers = set(openers_to_closers.keys())
     # create a set of the closers
+    closers = set(openers_to_closers.values())
     # initialize s to an instance of the Stack class
+    s = Stack()
     # loop through every character in code
+    for char in code:
         # if character is an opener
+        if char in openers:
             # push it onto the stack
+            s.push(char)
         # otherwise if character is a closer
+        elif char in closers:
             # pop off the stack
+            closer = s.pop()
             # if pop item does not match the current closer
+            if char != openers_to_closers[closer]:
                 # return False
+                return False
     # return True if the stack is empty
-    pass
+    return s.peek() is None
