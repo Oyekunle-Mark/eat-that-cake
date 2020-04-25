6a5113a75dd48eb8e27b7ece71803cd1496d9085
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-25 14:15:47 +0100

    Spaced out those blocks of code :)

--- queue_and_stack/get_closing_paren.py
+++ queue_and_stack/get_closing_paren.py
@@ -29,10 +29,12 @@ def get_closing_paren(sentence, opening_paren_index):
     if sentence[opening_paren_index] != '(':
         # raise an exception
         raise IndexError("Starting position must be an opening parenthesis")
+
     # instantiate the stack class
     s = Stack()
     # initialize sen_length to the length of the sentence
     sen_length = len(sentence)
+
     # loop from opening_paren_index to the end index of sentence
     for index in range(opening_paren_index, sen_length):
         # if character at current index is an opening parenthesis
@@ -43,10 +45,12 @@ def get_closing_paren(sentence, opening_paren_index):
         elif sentence[index] == ')':
             # pop from the stack
             s.pop()
+
         # if the stack is empty
         if s.peek() is None:
             # return current index
             return index
+
     # if the loop terminates without finding matching position
     # raise an exception
     raise IndexError("Starting position must be an opening parenthesis")
