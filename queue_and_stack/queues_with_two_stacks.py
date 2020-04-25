037e68161c543301318b3b6422909b2ffae49a92
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-25 08:54:42 +0100

    First pass pseudocode

--- queue_and_stack/queues_with_two_stacks.py
+++ queue_and_stack/queues_with_two_stacks.py
@@ -25,9 +25,24 @@ class Stack(object):
 
 
 class QueueTwoStacks(object):
+    # initialize a constructor
+        # set instance properties in_stack and out_stack
+        # to stack instances
 
     def enqueue(self, item):
+        # to enqueue, push onto the in_stack
         pass
 
     def dequeue(self):
+        # to dequeue, move all the items from the in_stack to the out_stack
+        # then pop off the out_stack
+
+        # first check if the out_stack is empty
+            # move all the items over from the in_stack
+            # loop while the in_stack is not empty
+                # get the popped item from the in_stack
+                # push that item onto the out_stack
+            # if the out_stack is still empty
+                # raise an exception
+        # return the popped item from the out_stack
         pass
