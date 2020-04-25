10cb157d714b083f23bcb4a3ccd5ea08ef33c803
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-25 14:09:16 +0100

    Implementation of the first pass

--- queue_and_stack/get_closing_paren.py
+++ queue_and_stack/get_closing_paren.py
@@ -26,14 +26,24 @@ class Stack(object):
 
 def get_closing_paren(sentence, opening_paren_index):
     # check if the starting parenthesis is a closing one
+    if sentence[opening_paren_index] != '(':
         # raise an exception
+        raise IndexError("Starting position must be an opening parenthesis")
     # instantiate the stack class
+    s = Stack()
     # initialize sen_length to the length of the sentence
+    sen_length = len(sentence)
     # loop from opening_paren_index to the end index of sentence
+    for index in range(opening_paren_index, sen_length):
         # if character at current index is an opening parenthesis
+        if sentence[index] == '(':
             # push it to the stack
+            s.push('(')
         # otherwise, if it is a closing parenthesis
+        elif sentence[index] == ')':
             # pop from the stack
+            s.pop()
         # if the stack is empty
+        if s.peek() is None:
             # return current index
-    pass
+            return index
