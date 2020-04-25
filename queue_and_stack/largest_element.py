a31833c86494e441b0f26d7f86cc1970d173f0d6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-25 08:08:54 +0100

    Implementation of the stack class

---
+++ queue_and_stack/largest_element.py
@@ -0,0 +1,24 @@
+class Stack(object):
+
+    def __init__(self):
+        """Initialize an empty stack"""
+        self.items = []
+
+    def push(self, item):
+        """Push a new item onto the stack"""
+        self.items.append(item)
+
+    def pop(self):
+        """Remove and return the last item"""
+        # If the stack is empty, return None
+        # (it would also be reasonable to throw an exception)
+        if not self.items:
+            return None
+
+        return self.items.pop()
+
+    def peek(self):
+        """Return the last item without removing it"""
+        if not self.items:
+            return None
+        return self.items[-1]
