8c0144936290fe6b63ef6896d9333aaf76d09023
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-27 20:26:01 +0100

    First pass implementation

--- linked_lists/contains_cycle.py
+++ linked_lists/contains_cycle.py
@@ -13,11 +13,18 @@ def contains_cycle(first_node):
     # when a node is found in the set already, return False
 
     # initialize nodes_visited to an empty set
+    nodes_visited = set()
     # set current to first_node
+    current = first_node
     # loop while current is not None
+    while current:
         # check if current is in nodes_visited
-            # return False
+        if current in nodes_visited:
+            # return True
+            return True
         # add current to nodes_visited
+        nodes_visited.add(current)
         # set current to current's next
+        current = current.next
     # return False
-    pass
+    return False
