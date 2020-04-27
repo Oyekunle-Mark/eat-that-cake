db5062d28d27facfb4bdc009cfc2efc28e858cba
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-27 17:44:43 +0100

    First pass pseudocode

--- linked_lists/delete_node.py
+++ linked_lists/delete_node.py
@@ -16,4 +16,18 @@ class LinkedListNode(object):
 
 
 def delete_node(node_to_delete):
+    # To delete a node from a Singly Linked List
+    # one way is to modify the previous node's next node property
+    # when given just the node to be deleted and there is no way
+    # of knowing the previous node, we can modify the node to be
+    # deleted.
+
+    # the two cases are that the node_to_delete can be the last
+    # node in the LinkedList or have nodes after it
+    # check if node_to_delete next is not None
+    # that is node_to_delete is not the tail
+        # set node_to_delete's value to the value of the next
+        # set node_to_delete's next to the next's next
+    # otherwise,
+        # point node_to_delete to None
     pass
