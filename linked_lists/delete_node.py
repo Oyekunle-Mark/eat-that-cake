8cf9cdb2ce4bcde3b8e19056bab65ef7b133522e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-27 17:35:16 +0100

    LinkedListNode class implementation

--- linked_lists/delete_node.py
+++ linked_lists/delete_node.py
@@ -1,9 +1,17 @@
 """
 class LinkedListNode(object):
 
-    def __init__(self, value):
-        self.value = value
-        self.next  = None
+        def __init__(self, value, next=None):
+            self.value = value
+            self.next  = next
+
+        def get_values(self):
+            node = self
+            values = []
+            while node is not None:
+                values.append(node.value)
+                node = node.next
+            return values
 """
 
 
