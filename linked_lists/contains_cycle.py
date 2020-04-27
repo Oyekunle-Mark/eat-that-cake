cb377f36a6042f85c6852b123141c1e239199f2e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-27 20:24:39 +0100

    First pass pseudocode

--- linked_lists/contains_cycle.py
+++ linked_lists/contains_cycle.py
@@ -8,4 +8,16 @@ class LinkedListNode(object):
 
 
 def contains_cycle(first_node):
+    # the plan would be to iterate through the linked list
+    # in one pass and add the nodes to a set as they are visited
+    # when a node is found in the set already, return False
+
+    # initialize nodes_visited to an empty set
+    # set current to first_node
+    # loop while current is not None
+        # check if current is in nodes_visited
+            # return False
+        # add current to nodes_visited
+        # set current to current's next
+    # return False
     pass
