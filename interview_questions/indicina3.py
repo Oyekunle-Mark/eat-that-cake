4d63756de7a49509bed634cc634aa53674ab7592
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-29 18:55:54 +0100

    Cleaned up the statement

--- interview_questions/indicina3.py
+++ interview_questions/indicina3.py
@@ -6,21 +6,31 @@ PROBLEM STATEMENT
 Write a function that converts number to text, Passing an int number should return array of text number.
 The function should chunk the int number into two digits and return the text format of the chunks.
 
-Example 1:
-Given:
+---------------------------------------------------------------------
 
+Example 1:
+Given 23498643: Function should return ['twenty-three', 'forty-nine', 'eighty-six', 'forty-three']
+
+Steps:
 23498643 => 23, 49, 86, 43 => ['twenty-three', 'forty-nine', 'eighty-six', 'forty-three']
 
-Example 2:
-Given:
+---------------------------------------------------------------------
 
+Example 2:
+Given 40263645: Function should return ['forty', 'twenty-six', 'thirty-six', 'forty-five']
+
+Steps:
 40263645 => 40, 26, 36, 45 => ['forty', 'twenty-six', 'thirty-six', 'forty-five']
 
-Example 3:
-Given:
+---------------------------------------------------------------------
 
+Example 3:
+Given 9203234612391071: Function should return ['ninety-two', 'three', 'twenty-three', 'forty-six', 'twelve', 'thirty-nine', 'ten', 'seventy-one']
+
+Steps
 9203234612391071 => 92, 03, 23, 46, 12, 39, 10, 71 => ['ninety-two', 'three', 'twenty-three', 'forty-six', 'twelve', 'thirty-nine', 'ten', 'seventy-one']
 
+---------------------------------------------------------------------
 
 NOTE:
 The number of digits in the input will always be even.
