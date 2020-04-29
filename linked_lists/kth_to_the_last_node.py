44a9be566bf94dac11257c4122be9f751807eb7c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-29 14:23:48 +0100

    Fixed a syntax error

--- linked_lists/kth_to_the_last_node.py
+++ linked_lists/kth_to_the_last_node.py
@@ -1,5 +1,5 @@
 def kth_to_last_node(k, head):
-    # this can be done is O(1) time and space
+    # this can be done is O(n) time and space
     # the approach will be to keep track of the node that was encountered
     # two nodes ago as we loop through the linked list
     # every node from the kth node onward, we set the kth to the last node
@@ -10,6 +10,7 @@ def kth_to_last_node(k, head):
     kth_to_last = head
     # initialize current to the head
     current = head
+
     # loop through all the nodes
     while current:
         # increment count by one
@@ -20,9 +21,11 @@ def kth_to_last_node(k, head):
         if count > k:
             # set kth_to_last to the next node after it.
             kth_to_last = kth_to_last.next
+
     # if count is less than k
     if k == 0 or count < k:
         # raise an exception
         raise Exception("K cannot be zero or greater than Linked List length")
+
     # return kth_to_last
     return kth_to_last
