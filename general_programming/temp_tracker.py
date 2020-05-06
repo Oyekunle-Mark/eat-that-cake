dcc45b1d03f143efab5e629ba83bdb47e0f62466
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-06 15:01:14 +0100

    Pseudocode for get max and min

--- general_programming/temp_tracker.py
+++ general_programming/temp_tracker.py
@@ -1,14 +1,21 @@
 class TempTracker(object):
     def __init__(self):
+        # initialize max_temp and min_temp to negative and positive infinities
         pass
 
     def insert(self, temperature):
+        # if temperature is greater than max_temp
+            # set max_temp to temperature
+        # otherwise if temperature is lower than min_temp
+            # set min_temp to temperature
         pass
 
     def get_max(self):
+        # return max_temp
         pass
 
     def get_min(self):
+        # return min_temp
         pass
 
     def get_mean(self):
