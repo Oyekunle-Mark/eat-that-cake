1186d58d504f180a7cb522c26716cf89c9a8952f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-06 15:03:51 +0100

    implemented min max method

--- general_programming/temp_tracker.py
+++ general_programming/temp_tracker.py
@@ -1,22 +1,27 @@
 class TempTracker(object):
     def __init__(self):
         # initialize max_temp and min_temp to negative and positive infinities
+        self.max_temp = float('-inf')
+        self.min_temp = float('inf')
         pass
 
     def insert(self, temperature):
         # if temperature is greater than max_temp
+        if temperature > self.max_temp:
             # set max_temp to temperature
+            self.max_temp = temperature
         # otherwise if temperature is lower than min_temp
+        elif temperature < self.min_temp:
             # set min_temp to temperature
-        pass
+            self.min_temp = temperature
 
     def get_max(self):
         # return max_temp
-        pass
+        return self.max_temp
 
     def get_min(self):
         # return min_temp
-        pass
+        return self.min_temp
 
     def get_mean(self):
         pass
