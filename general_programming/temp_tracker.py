dd7ac77401cd375f7dce7e7a55820ed52088d3a0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-06 14:56:24 +0100

    Boiler plate code

---
+++ general_programming/temp_tracker.py
@@ -0,0 +1,15 @@
+class TempTracker(object):
+    def insert(self, temperature):
+        pass
+
+    def get_max(self):
+        pass
+
+    def get_min(self):
+        pass
+
+    def get_mean(self):
+        pass
+
+    def get_mode(self):
+        pass
