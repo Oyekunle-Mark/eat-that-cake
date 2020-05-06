218b0f224f61328845c1eeb0148ffe1eae6d6344
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-06 15:46:51 +0100

    Returned back to infinities but separated the if conditionals

--- general_programming/temp_tracker.py
+++ general_programming/temp_tracker.py
@@ -1,8 +1,8 @@
 class TempTracker(object):
     def __init__(self):
         # initialize max_temp and min_temp to None
-        self.max_temp = None
-        self.min_temp = None
+        self.min_temp = float('inf')
+        self.max_temp = float('-inf')
         # initialize mean, sum_so_far and count to zero
         self.mean, self.sum_so_far, self.count = 0, 0, 0
         # initialize mode to zero
@@ -12,11 +12,6 @@ class TempTracker(object):
         self.temp_frequency = [0] * 111
 
     def insert(self, temperature):
-        # if self.max_temp and self.min_temp is None
-        if self.max_temp is None and self.min_temp is None:
-            # set both to temperature
-            self.max_temp, self.min_temp = temperature, temperature
-
         # increment count by one
         self.count += 1
         # add temperature to  sum_so_far
@@ -37,7 +32,7 @@ class TempTracker(object):
             # set max_temp to temperature
             self.max_temp = temperature
         # otherwise if temperature is lower than min_temp
-        elif temperature < self.min_temp:
+        if temperature < self.min_temp:
             # set min_temp to temperature
             self.min_temp = temperature
 
