e1a3d28115ccab6e990b821ad2f3b795edecabee
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-06 14:38:49 +0100

    completed the implementation of the first pass

--- general_programming/find_rectangular_overlap.py
+++ general_programming/find_rectangular_overlap.py
@@ -33,9 +33,25 @@ def find_overlap(point1, length1, point2, length2):
 def find_rectangular_overlap(rect1, rect2):
     # initialize left_x and width by destructuring the return value of
     # find_overlap passing in left_x and width of rect1 and rect2
+    left_x, width = find_overlap(
+        rect1['left_x'], rect1['width'], rect2['left_x'], rect2['width'])
     # initialize bottom_y and height by destructuring the return value of
     # find_overlap passing in bottom_y and height of rect1 and rect2
+    bottom_y, height = find_overlap(
+        rect1['bottom_y'], rect1['height'], rect2['bottom'], rect2['height'])
     # if left_x and bottom_y are None
+    if left_x is None or bottom_y is None:
         # return an object with None for all required properties
+        return {
+            'left_x': None,
+            'bottom_y': None,
+            'width': None,
+            'height': None,
+        }
     # return an object with with the return values of find_overlap
-    pass
+    return {
+        'left_x': left_x,
+        'bottom_y': bottom_y,
+        'width': width,
+        'height': height,
+    }
