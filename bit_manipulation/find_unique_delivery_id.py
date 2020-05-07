b5274ddb2e702f23d4fc51f5bb7e719217cc3ae4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-07 17:19:40 +0100

    First pass pseudocode

--- bit_manipulation/find_unique_delivery_id.py
+++ bit_manipulation/find_unique_delivery_id.py
@@ -1,2 +1,12 @@
 def find_unique_delivery_id(delivery_ids):
+    # a simple way of doing this in linear time
+    # and constant space is to cancel out the repeated delivery_id
+    # to cancel out two numbers you can use bitwise xor
+
+    # initialize unique_id to zero
+    # loop through every delivery_id in delivery_ids
+        # xor delivery_id and unique_id
+    # whatever number is left at the end of loop is the unique_id
+    # xor has cancelled out all repeated delivery_id
+    # return unique_id
     pass
