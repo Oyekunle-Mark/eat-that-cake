6241b9a929f7692d8bf22771fe210a886052d359
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-09 16:50:23 +0100

    Pseudocode for first pass

--- combinatorics_probability_math/rand7_to_rand5.py
+++ combinatorics_probability_math/rand7_to_rand5.py
@@ -6,10 +6,15 @@ def rand7():
 
 
 def rand5():
+    # since every number in the five-sided die must have the same
+    # probability of being chosen
+    # we just keep on rolling the die until we get a number
+    # that is five or less
 
-    # Implement rand5() using rand7()
-
-    return 0
+    # initialize result to the return value of rand7
+    # return result if it is less than or equal to 5
+    # otherwise return the recursive call to rand5
+    pass
 
 
 print('Rolling 5-sided die...')
