fe2858502f337533dd124c2081cbbbb58ad6f9f2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-09 16:47:16 +0100

    Problem template

---
+++ combinatorics_probability_math/rand7_to_rand5.py
@@ -0,0 +1,16 @@
+import random
+
+
+def rand7():
+    return random.randint(1, 7)
+
+
+def rand5():
+
+    # Implement rand5() using rand7()
+
+    return 0
+
+
+print('Rolling 5-sided die...')
+print(rand5())
