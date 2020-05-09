393a24ee7bf41dd4c27c278ca0b454bfa1bd34e8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-09 16:18:01 +0100

    Pseudocode for the first pass

--- combinatorics_probability_math/find_repeat.py
+++ combinatorics_probability_math/find_repeat.py
@@ -1,2 +1,13 @@
 def find_repeat(numbers_list):
+    # to save up space, this can be done in O(1) space
+    # since 1..n is a triangular series
+    # we can add up every number in the series using the formula
+    # add up all the numbers. The difference between the two
+    # will be the repeated number
+
+    # find the value of n which will be the length minus
+    # one because of the repeat number
+    # find the triangular_sum using the formula
+    # find the total_sum of all numbers in numbers_list
+    # return the difference of total_sum and triangular_sum
     pass
