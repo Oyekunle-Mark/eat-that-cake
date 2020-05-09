c26dea297d31b17b3200e83e71f9c16b45beb701
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-09 16:19:09 +0100

    Implementation of first pass

--- combinatorics_probability_math/find_repeat.py
+++ combinatorics_probability_math/find_repeat.py
@@ -7,7 +7,10 @@ def find_repeat(numbers_list):
 
     # find the value of n which will be the length minus
     # one because of the repeat number
+    n = len(numbers_list) - 1
     # find the triangular_sum using the formula
+    triangular_sum = (n * n + n) / 2
     # find the total_sum of all numbers in numbers_list
+    total_sum = sum(numbers_list)
     # return the difference of total_sum and triangular_sum
-    pass
+    return total_sum - triangular_sum
