89b4ee8aa2cffd97bbbfaee880cf3c1db8e156e9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-09 16:40:22 +0100

    Implementation of the first pass

--- combinatorics_probability_math/contains.py
+++ combinatorics_probability_math/contains.py
@@ -3,14 +3,24 @@ def contains(ordered_list, number):
     # use binary search and find the number(or not) in O(log n)
 
     # initialize lower to zero
+    lower = 0
     # initialize upper to length of ordered_list minus one
+    upper = len(ordered_list) - 1
     # loop while lower <= upper
+    while lower <= upper:
         # initialize midpoint to the average of lower and upper
+        midpoint = (lower + upper) // 2
         # if number is equivalent to value at midpoint
+        if number == ordered_list[midpoint]:
             # return True
+            return True
         # if number is greater than value at midpoint
+        if number > ordered_list[midpoint]:
             # set lower to midpoint plus one
+            lower = midpoint + 1
         # otherwise
+        else:
             # set upper to midpoint minus one
+            upper = midpoint - 1
     # return False
-    pass
+    return False
