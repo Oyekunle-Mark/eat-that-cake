85b46ae5e7aad38d0827ac68ae7492a95ced74a3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-09 16:51:31 +0100

    First pass implementation

--- combinatorics_probability_math/rand7_to_rand5.py
+++ combinatorics_probability_math/rand7_to_rand5.py
@@ -12,9 +12,10 @@ def rand5():
     # that is five or less
 
     # initialize result to the return value of rand7
+    result = rand7()
     # return result if it is less than or equal to 5
     # otherwise return the recursive call to rand5
-    pass
+    return result if result <= 5 else rand5()
 
 
 print('Rolling 5-sided die...')
